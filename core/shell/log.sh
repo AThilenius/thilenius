391f05199da7f4053d506d1d0bba2240e26be2b2
Alec Thilenius <alec@thilenius.com>
2015-06-25 17:56:05 -0700

    Renamed dev_env to dev and added support for nested docker within docker.

--- core/shell/log.sh
+++ core/shell/log.sh
@@ -5,12 +5,12 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 function INFO {
-  printf "${GREEN}[INFO]${NC} $(date): $1\n"
-} 
+  printf "${GREEN}[INFO]${NC}: $1\n"
+}
 
 function WARN {
-  printf "${YELLOW}[WARN]${NC} $(date): $1\n"
-} 
+  printf "${YELLOW}[WARN]${NC}: $1\n"
+}
 
 function EROR {
   printf "${RED}[EROR] $(date): $1${NC}\n"
