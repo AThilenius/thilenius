82edca7089637a96dc02e43debdea104dfbfead9
Alec Thilenius <alec@thilenius.com>
2015-07-29 22:51:51 -0700

    Added gflags moved base from libs/cpp/base to /base. Fixed all compilation.

--- scorch/anvil/anvil.cc
+++ scorch/anvil/anvil.cc
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "base/gflags.h"
 #include "base/string.h"
 
-int main() {
+DEFINE_string(test, "Defaul value", "A test flag!");
+
+int main(int argc, char **argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
   std::cout << "Hello, world" << std::endl;
+  std::cout << "Got flag: " << FLAGS_test << std::endl;
 }
