240dba5f82b4d55411f719fb40e6985e18f1e7f0
Alec Thilenius <alec@thilenius.com>
2015-07-30 18:49:28 -0700

    Got all desired Google libraries linked in properly.

--- scorch/anvil/anvil.cc
+++ scorch/anvil/anvil.cc
@@ -1,11 +1,28 @@
 #include <iostream>
-#include "base/gflags.h"
+#include "base/gflags/gflags.h"
+#include "base/glog/logging.h"
+#include "base/gmock/gmock.h"
+#include "base/gtest/gtest.h"
 #include "base/string.h"
 
 DEFINE_string(test, "Defaul value", "A test flag!");
 
+TEST(FactorialTest, Zero) { EXPECT_EQ(1, 1); }
+
 int main(int argc, char **argv) {
+  // Google Flags
   gflags::ParseCommandLineFlags(&argc, &argv, true);
-  std::cout << "Hello, world" << std::endl;
-  std::cout << "Got flag: " << FLAGS_test << std::endl;
+
+  // GoogleLog
+  google::InitGoogleLogging(argv[0]);
+  FLAGS_colorlogtostderr = true;
+  FLAGS_logtostderr = true;
+
+  // GoogleTest / GoogleMcok
+  ::testing::InitGoogleMock(&argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+
+  LOG(INFO) << "Hello from GLOG";
+
+  return RUN_ALL_TESTS();
 }
